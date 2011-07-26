f657e78c6b6518445261beb3c2565481d641c241
Raffael Herzog <herzog@raffael.ch>
2011-07-26 22:33:03 +0200

    Add bindings to first step of create certificate wizard.

--- src/main/java/ch/piratenpartei/pivote/ui/cert/CertificateBaseData.java
+++ src/main/java/ch/piratenpartei/pivote/ui/cert/CertificateBaseData.java
@@ -14,19 +14,29 @@ import javax.swing.JTextField;
 
 import ch.piratenpartei.pivote.DevelUtilities;
 import ch.piratenpartei.pivote.HelpResources;
+import ch.piratenpartei.pivote.model.Certificate;
 import ch.piratenpartei.pivote.ui.AbstractActivity;
 import com.intellij.uiDesigner.core.GridConstraints;
 import com.intellij.uiDesigner.core.GridLayoutManager;
 import com.intellij.uiDesigner.core.Spacer;
 
+import ch.raffael.util.binding.PresentationModel;
+import ch.raffael.util.binding.SimpleBinding;
+import ch.raffael.util.binding.validate.ValidationEvent;
+import ch.raffael.util.binding.validate.ValidationListener;
 import ch.raffael.util.i18n.I18N;
 import ch.raffael.util.swing.actions.CommonAction;
+import ch.raffael.util.swing.binding.TextComponentAdapter;
+import ch.raffael.util.swing.binding.ValidationFeedbackManager;
+import ch.raffael.util.swing.components.feedback.FeedbackPanel;
+
+import static ch.raffael.util.binding.validate.Validators.*;
 
 
 /**
  * @author <a href="mailto:herzog@raffael.ch">Raffael Herzog</a>
  */
-public class CertificateBaseData extends AbstractActivity<JPanel> {
+public class CertificateBaseData extends AbstractActivity<JComponent> {
 
     private static Resources res = I18N.getBundle(Resources.class);
 
@@ -35,10 +45,28 @@ public class CertificateBaseData extends AbstractActivity<JPanel> {
     private JTextField email;
     private JPanel root;
 
+    private final PresentationModel pmodel = new PresentationModel();
+
+    private final SimpleBinding<Certificate> certificate = pmodel.add(new SimpleBinding<Certificate>(new Certificate()));
+
     private boolean closeable = false;
 
     public CertificateBaseData() {
         super("Create New Certificate", "Blah blah blah", I18N.getBundle(HelpResources.class).meta().resource(URL.class, "someHelp"));
+        pmodel.addValidationListener(new ValidationFeedbackManager());
+        pmodel.add(new TextComponentAdapter())
+                .install(firstName, certificate.<String>property("firstName")
+                        .buffer()
+                        .validator(notEmpty()));
+        pmodel.add(new TextComponentAdapter())
+                .install(lastName, certificate.<String>property("lastName")
+                        .buffer()
+                        .validator(notEmpty()));
+        pmodel.add(new TextComponentAdapter())
+                .install(email, certificate.<String>property("lastName")
+                        .buffer()
+                        .validator(notEmpty().and(email())));
+        pmodel.scheduleInitialValidation(root);
     }
 
     @Override
@@ -52,8 +80,18 @@ public class CertificateBaseData extends AbstractActivity<JPanel> {
     }
 
     @Override
-    protected JPanel buildUi() {
+    protected JComponent buildUi() {
         actions(new CommonAction("Continue", res.forwardIcon()) {
+            @Override
+            protected void init() {
+                pmodel.addValidationListener(new ValidationListener() {
+                    @Override
+                    public void validationChanged(ValidationEvent event) {
+                        setEnabled(pmodel.isValid());
+                    }
+                });
+                setEnabled(pmodel.isValid());
+            }
             @Override
             public void actionPerformed(ActionEvent e) {
                 try {
@@ -76,7 +114,8 @@ public class CertificateBaseData extends AbstractActivity<JPanel> {
                 }
             }
         });
-        return root;
+        FeedbackPanel feedbackPanel = new FeedbackPanel(root);
+        return feedbackPanel;
     }
 
     {
